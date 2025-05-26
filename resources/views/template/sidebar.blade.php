e9d066d8ce5dfb2428c79b91d29906f7db303c66
zikrihasbi <jackhsb22@gmail.com>
2025-05-26 17:16:34 +0800

    template view

---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,47 @@
+      <!--begin::Sidebar-->
+      <aside class="app-sidebar bg-body-secondary shadow" data-bs-theme="dark">
+        <!--begin::Sidebar Brand-->
+        <div class="sidebar-brand">
+          <!--begin::Brand Link-->
+          <a href="./index.html" class="brand-link">
+            <!--begin::Brand Image-->
+            <img
+              src="dist/assets/img/AdminLTELogo.png"
+              alt="AdminLTE Logo"
+              class="brand-image opacity-75 shadow"
+            />
+            <!--end::Brand Image-->
+            <!--begin::Brand Text-->
+            <span class="brand-text fw-light">AdminLTE 4</span>
+            <!--end::Brand Text-->
+          </a>
+          <!--end::Brand Link-->
+        </div>
+        <!--end::Sidebar Brand-->
+        <!--begin::Sidebar Wrapper-->
+        <div class="sidebar-wrapper">
+          <nav class="mt-2">
+            <!--begin::Sidebar Menu-->
+            <ul
+              class="nav sidebar-menu flex-column"
+              data-lte-toggle="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li class="nav-item">
+                <a href="index.php" class="nav-link">
+                  <i class="nav-icon bi bi-palette"></i>
+                  <p>Mahasiswa</p>
+                </a>
+                <li class="nav-item">
+                <a href="prodi.php" class="nav-link">
+                  <i class="nav-icon bi bi-palette"></i>
+                  <p>Program Studi</p>  
+                </a>
+            </ul>
+            <!--end::Sidebar Menu-->
+          </nav>
+        </div>
+        <!--end::Sidebar Wrapper-->
+      </aside>
+      <!--end::Sidebar-->
